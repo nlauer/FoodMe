8bc63a088ccca9eb59598ef8dfeb9c314ba54d72
nlauer <nlauer9@gmail.com>
2013-03-02 13:04:43 -0800

    Initial commit

---
+++ FoodMe/FoodMe/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FoodMe
+//
+//  Created by Nick Lauer on 13-03-02.
+//  Copyright (c) 2013 NickLauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FoodMe/FoodMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FoodMe
+//
+//  Created by Nick Lauer on 13-03-02.
+//  Copyright (c) 2013 NickLauer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
