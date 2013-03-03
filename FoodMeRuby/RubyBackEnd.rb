0dc5841df355696276a44ad370e48a1a5afe8384
jhysum <sum_jesse@hotmail.com>
2013-03-02 16:28:51 -0800

    added the get and post function

--- FoodMeRuby/RubyBackEnd.rb
+++ FoodMeRuby/RubyBackEnd.rb
@@ -1,6 +1,21 @@
 require "sinatra"
 
-get'/' do
+get'/Registration' do
 	"Hello World!"
 end
 
+get '/Login' do
+  "You're logging in #{params[:name]} and #{params[:something]} "
+end
+
+post '/Login/:name&password' do
+
+end
+
+post '/Registration' do
+
+end
+
+post '/Order' do
+
+end
