f00fe4dec8b68684b70a894aca3004883d482255
jhysum <sum_jesse@hotmail.com>
2013-03-02 18:54:20 -0800

    added create user and ordrin key

--- FoodMeRuby/RubyBackEnd.rb
+++ FoodMeRuby/RubyBackEnd.rb
@@ -1,6 +1,11 @@
 require "sinatra"
 require "ordrin"
 
+API_KEY = "U-o5Jb1VJ6Odx4Z8qEU3EGt3_xCrO7G2gfZBdsKysEA"
+SECRET_API_KEY = "b6MDqRkxBDZs6l9ArgOZEdb0ifbYobSUXlzdX4hRi5M"
+
+api = Ordrin::APIs.new(SECRET_API_KEY, :test)
+
 set :views, settings.root + '/../views'
 set :public_folder, File.dirname(__FILE__) + '/../static'
 
@@ -21,7 +26,9 @@ post '/login/:name&password' do
 end
 
 post '/register/complete/?' do
-  "params #{params}"
+  login = Ordrin::Data::UserLogin.new(params[:email],params[:password])
+  puts api.user.create(login,params[:first_name],params[:last_name])
+  "#{api.user.get(login)}"
 end
 
 post '/order' do
