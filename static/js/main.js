f7ad3fca141ef5666e8d5bb3add549300e78d821
Alexey Komissarouk <alexey@alexeymk.com>
2013-03-02 19:35:38 -0800

    we're now pretending to be logging in, storing via a client-side cookie

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,18 @@
 $('#signin_form').submit(function() {
-// TODO validate
+  // TODO validate
+  // yeah, this is a pretty terrible way to do this. #hackathonlife
   monster.set('user_creds', {
     email: $('#signin_email').val(),
     password: $('#signin_password').val()
   });
-  return false // do not continue!
+});
+
+$(document).on('ready', function() {
+  var user_info = monster.get('user_creds');
+  if (user_info) {
+    // user is logged in
+    $('#user_email').html(user_info.email);
+    $('#user_logged_in').show();
+    $('#signin_form').hide();
+  }
 });
